3be3936e9d904bfd908226da5f796d130bde8951
emrht <emrhtfn@gmail.com>
2023-11-05 23:21:54 +0300

    hot fix

--- main.tf
+++ main.tf
@@ -12,8 +12,6 @@ module "eks_cluster" {
   source = "./eks_cluster"
 
   eks_subnet_ids   = flatten([module.eks_vpc.pub_subnets, module.eks_vpc.priv_subnets])
-  eks_subnet_id_node_group1   = flatten([module.eks_vpc.pub_subnets[0], module.eks_vpc.priv_subnets[0]])
-  eks_subnet_id_node_group2   = flatten([module.eks_vpc.pub_subnets[1], module.eks_vpc.priv_subnets[1]])
   eks_cluster_name = var.eks_cluster_name
   cluster_role     = var.cluster_role
 
@@ -26,6 +24,8 @@ module "eks_node_group" {
   source = "./eks_node_group"
 
   eks_subnet_ids    = flatten([module.eks_vpc.pub_subnets, module.eks_vpc.priv_subnets])
+  eks_subnet_id_node_group1   = flatten([module.eks_vpc.pub_subnets[0], module.eks_vpc.priv_subnets[0]])
+  eks_subnet_id_node_group2   = flatten([module.eks_vpc.pub_subnets[1], module.eks_vpc.priv_subnets[1]])
   eks_cluster_name  = module.eks_cluster.cluster_name
   node_group_name1   = var.node_group_name1
   node_group_name2   = var.node_group_name2
